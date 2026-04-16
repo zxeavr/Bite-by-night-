fe2319cf9c09917ca8843e23ba5425033967c4bf
Pls donate <mra181958@gmail.com>
2026-04-16 19:04:46 +0300

    Update and rename version.lua to version.text

--- version.lua
+++
@@ -1,8 +0,0 @@
---[[
-  Phantom Hub Version File
-  قم بتحديث الأرقام التالية عند كل إصدار جديد
---]]
-return {
-    version = 1,   -- رقم الإصدار الحالي
-    hash = "7d34ac13f2aa8674dcbebf0fe23c29fff6323f7a51365cb300c495d34b01ffdc" -- SHA256 hash
-}
